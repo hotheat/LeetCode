9ed6b5875e8bc9553b6a655fb39251824d60f3f8
hotheat <guoam630@outlook.com>
2019-01-30 13:22:37 +0800

    8.string to integer

---
+++ 8. String to Integer (atoi)/8.py
@@ -0,0 +1,35 @@
+class Solution:
+    def myAtoi(self, str):
+        """
+        :type str: str
+        :rtype: int
+        """
+        num = ''
+        for i, v in enumerate(str):
+            # 保证第一个字符是 - 或 +，而且后面不再添加 + 或 -
+            if not num and v in '+-':
+                num = ''.join((num, v))
+                continue
+            # 符合数字
+            if 48 <= ord(v) <= 57:
+                num = ''.join((num, v))
+            # 保证数字是连续出现
+            elif ord(v) == 32 and not num:
+                continue
+            else:
+                break
+
+        # 不是空字符串或单独出现 + 或 -时
+        if num and num != '+' and num != '-':
+            if int(num) >= 0:
+                return min(int(num), 2 ** 31 - 1)
+            elif int(num) < 0:
+                return max(int(num), -2 ** 31)
+        else:
+            return 0
+
+
+if __name__ == '__main__':
+    for s in ['+0 123', '+12 34', "4193 with words", "   -4  2", "words and 987", "-91283472332"]:
+        sl = Solution()
+        print(sl.myAtoi(s))
