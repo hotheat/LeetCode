6d7eb00d77b284b064e3a85f0173bf34512aaee9
hotheat <guoam630@outlook.com>
2019-01-31 10:12:26 +0800

    14-2

--- 14. Longest Common Prefix/14.py
+++ 14. Longest Common Prefix/14.py
@@ -10,15 +10,14 @@ class Solution:
             return strs[0]
         for i in range(0, len(strs) - 1):
             res = ''
-            fir, sec = 0, 0
+            fir = 0
 
-            while fir < len(strs[i]) and sec < len(strs[i + 1]):
-                if strs[i][fir] == strs[i + 1][sec]:
-                    res = ''.join((res, strs[i][sec]))
+            while fir < len(strs[i]) and fir < len(strs[i + 1]):
+                if strs[i][fir] == strs[i + 1][fir]:
+                    res = ''.join((res, strs[i][fir]))
                 else:
                     break
                 fir += 1
-                sec += 1
             if not res:
                 break
             # 将下一组要判断的第一个字符串记为 res
