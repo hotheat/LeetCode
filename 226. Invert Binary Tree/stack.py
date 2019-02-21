9c42e93b12df31c13e3aa672370e4a562502be6f
hotheat <guoam630@outlook.com>
2019-02-21 13:49:44 +0800

    226

---
+++ 226. Invert Binary Tree/stack.py
@@ -0,0 +1,19 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+from collections import deque
+
+
+class Solution:
+    def invertTree(self, root: 'TreeNode') -> 'TreeNode':
+        d = deque([root])
+        while d:
+            tmp = d.pop()
+            if tmp:
+                tmp.left, tmp.right = tmp.right, tmp.left
+                d.append(tmp.left)
+                d.append(tmp.right)
+        return root
