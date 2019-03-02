5358fded7f7d233735381f74415eda8af7be6f14
hotheat <guoam630@outlook.com>
2019-03-02 21:56:04 +0800

    043

---
+++ 043.Multiply Strings/043.py
@@ -0,0 +1,21 @@
+class Solution:
+    def multiply(self, num1: str, num2: str) -> str:
+        # 最终结果不可能超过 len(num1) + len(num2) 的
+        res = [0] * (len(num1) + len(num2))
+
+        for i in range(len(num2) - 1, -1, -1):
+            for j in range(len(num1) - 1, -1, -1):
+                low = i + j + 1
+                high = i + j
+                mul = int(num2[i]) * int(num1[j])
+                mul += res[low]
+                res[low] = mul % 10
+                res[high] += mul // 10
+
+        mul = ''.join([str(i) for i in res])
+
+        return '0' if not mul else mul[mul.find('0')+1:]
+
+
+if __name__ == '__main__':
+    print(Solution().multiply('999', '999'))
